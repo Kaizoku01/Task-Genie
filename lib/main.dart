18b3eddceef979c14b590a091df83ed73aa2fcc4
sarvagya <9920103123@mail.jiit.ac.in>
2023-12-30 17:18:37 +0530

    renamed

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TasksScreen(),
     );
   }
